e3f41546ac157fc7c77dc358a8eecd9ad87a0384
roopeshkewlani <roopesh.kewlani90@gmail.com>
2020-10-03 06:53:18 -0700

    Added DTO

---
+++ customer/customer/src/main/java/com/indie/farms/customer/dto/User.java
@@ -0,0 +1,14 @@
+package com.indie.farms.customer.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String name;
+    private String emailId;
+    private String password;
+    private Long contactNo;
+}
